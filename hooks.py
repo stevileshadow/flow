022b2b53c2958095b4490e0b14f5286484e47ff0
stevileshadow <virgilesoglo@gmail.com>
2026-03-23 17:01:30 -0400

    Add initial app configuration for Flow
    hooks.py

---
+++ hooks.py
@@ -0,0 +1,10 @@
+app_name = "flow"
+app_title = "Flow"
+app_publisher = "stevileshadow"
+app_description = "Gestion de flotte et mandats de signalisation"
+app_icon = "octicon octicon-zap"
+app_color = "blue"
+app_email = "ton-courriel@exemple.com"
+app_license = "mit"
+
+# C'est ici que Flow viendra s'insérer dans ERPNext
